353b8b86f40c2f43e0cf7cce77160268a5dff037
lucasluccaroni <lucas_luccaroni@hotmail.com.ar>
2024-09-26 09:51:22 -0300

    adicion de variables de entorno a vercel en produccion para correcta visualizacion del deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import Navbar from "./components/Navbar/Navbar"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CartProvider } from "./context/CartContext"
 import CartView from "./components/CartView/CartView"
-import { NotificationProvider} from "./notification/NotificationService"
+import { NotificationProvider } from "./notification/NotificationService"
 import Checkout from "./components/Checkout/Checkout"
 
 
@@ -12,16 +12,16 @@ function App() {
   return (
     <>
       <NotificationProvider>
-        <BrowserRouter> 
+        <BrowserRouter>
           <CartProvider>
-            <Navbar/>
+            <Navbar />
             <Routes>
               <Route path="/" element={<ItemListContainer greeting="Todos los productos" />} />
               <Route path="/category/:categoryId" element={<ItemListContainer greeting="Productos filtrados por categoria" />} />
               <Route path="/detail/:productId" element={<ItemDetailContainer />} />
-              <Route path="/cart" element={<CartView/>}/>
-              <Route path="/checkout" element={<Checkout/>}/>
-              <Route path="*" element={<h1>404 Not Found</h1>}/>
+              <Route path="/cart" element={<CartView />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<h1>404 Not Found</h1>} />
             </Routes>
           </CartProvider>
         </BrowserRouter>
